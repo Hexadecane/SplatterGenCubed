f978be892f1ceaf61998586006e841c54e33f2a5
Hexadecane <36948730+Hexadecane@users.noreply.github.com>
2021-06-02 02:49:29 -0400

    Initial commit
    The initial commit of SplatterGenCubed.

---
+++ Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace SplatterGenCubed {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.AppPictureBox = new System.Windows.Forms.PictureBox();
+            this.SelectionPanel = new System.Windows.Forms.Panel();
+            this.OptionsButton = new System.Windows.Forms.Button();
+            this.GenerateButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.AppPictureBox)).BeginInit();
+            this.SelectionPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(280, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // AppPictureBox
+            // 
+            this.AppPictureBox.Location = new System.Drawing.Point(12, 27);
+            this.AppPictureBox.Name = "AppPictureBox";
+            this.AppPictureBox.Size = new System.Drawing.Size(256, 256);
+            this.AppPictureBox.TabIndex = 1;
+            this.AppPictureBox.TabStop = false;
+            // 
+            // SelectionPanel
+            // 
+            this.SelectionPanel.Controls.Add(this.OptionsButton);
+            this.SelectionPanel.Controls.Add(this.GenerateButton);
+            this.SelectionPanel.Controls.Add(this.SaveButton);
+            this.SelectionPanel.Location = new System.Drawing.Point(12, 289);
+            this.SelectionPanel.Name = "SelectionPanel";
+            this.SelectionPanel.Size = new System.Drawing.Size(256, 23);
+            this.SelectionPanel.TabIndex = 3;
+            // 
+            // OptionsButton
+            // 
+            this.OptionsButton.Location = new System.Drawing.Point(177, 0);
+            this.OptionsButton.Name = "OptionsButton";
+            this.OptionsButton.Size = new System.Drawing.Size(76, 23);
+            this.OptionsButton.TabIndex = 0;
+            this.OptionsButton.Text = "Options";
+            this.OptionsButton.UseVisualStyleBackColor = true;
+            this.OptionsButton.Click += new System.EventHandler(this.OptionsButton_Click);
+            // 
+            // GenerateButton
+            // 
+            this.GenerateButton.Location = new System.Drawing.Point(90, 0);
+            this.GenerateButton.Name = "GenerateButton";
+            this.GenerateButton.Size = new System.Drawing.Size(76, 23);
+            this.GenerateButton.TabIndex = 0;
+            this.GenerateButton.Text = "Generate";
+            this.GenerateButton.UseVisualStyleBackColor = true;
+            this.GenerateButton.Click += new System.EventHandler(this.GenerateButton_Click);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(3, 0);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(76, 23);
+            this.SaveButton.TabIndex = 0;
+            this.SaveButton.Text = "Save Decal";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(280, 324);
+            this.Controls.Add(this.SelectionPanel);
+            this.Controls.Add(this.AppPictureBox);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "SplatterGenCubed";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.AppPictureBox)).EndInit();
+            this.SelectionPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.PictureBox AppPictureBox;
+        private System.Windows.Forms.Panel SelectionPanel;
+        private System.Windows.Forms.Button OptionsButton;
+        private System.Windows.Forms.Button GenerateButton;
+        private System.Windows.Forms.Button SaveButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SplatterGenCubed {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        // Options form stuff:
+        OptionsForm OF = new OptionsForm();
+
+        // Image settings:
+        public Color bgColor = Color.FromArgb(255, 127, 127, 127);
+        private int imgWidth;
+        private int imgHeight;
+        private int centerX;
+        private int centerY;
+
+        // Random number generator:
+        static public Random rng = new Random();
+        // Data that will be output upon exporting:
+        public Image DecalOutputData { get; set; }
+
+
+
+        private void GenerateButton_Click(object sender, EventArgs e) {
+
+        }
+
+
+        private void SaveButton_Click(object sender, EventArgs e) {
+
+        }
+
+
+        private void OptionsButton_Click(object sender, EventArgs e) {
+            OF.ShowDialog();
+        }
+    }
+
+}
